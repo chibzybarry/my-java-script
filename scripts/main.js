1b40f231c63ea027915b64a3eca4267122063f53
chibzybarry <osunwachibueze@gmail.com>
2024-09-23 10:15:49 +0100

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+// Reverse a String: Write a function that reverses a given string.
+
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+
+const myinputString = "Emmanuel osunwa";
+const reversedString = reverseString (myinputString);
+console.log (reversedString);
+
+
+
+function CountNumbers() {
+    console.log .length;
+
+}
+
+
+const inputString = "emmanuel osunwa";
+const characterCount = CountNumbers(inputString);
+console.log(characterCount);
+
+
+
+
+
